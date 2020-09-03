ee41e7d6f91c57dc052730f75debdcbd2384d323
MJ11 <gunpollo1886@gmail.com>
2020-09-03 14:20:18 +0900

    whatthehelll

---
+++ prototype/Today/morningCeremonyPrayerViewController.swift
@@ -0,0 +1,21 @@
+//
+//  morningCeremonyPrayerViewController.swift
+//  prototype
+//
+//  Created by 김민준 on 2020/09/03.
+//  Copyright © 2020 김민준. All rights reserved.
+//
+
+import UIKit
+
+class morningCeremonyPrayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ prototype/Today/morningCeremonyViewController.swift
@@ -0,0 +1,19 @@
+//
+//  morningCeremonyViewController.swift
+//  prototype
+//
+//  Created by 김민준 on 2020/09/02.
+//  Copyright © 2020 김민준. All rights reserved.
+//
+
+import UIKit
+
+class morningCeremonyQuestionViewController: UIViewController {
+
+    @IBOutlet weak var questionNumber: UILabel!
+    
+    @IBOutlet weak var morningQuestion: UILabel!
+    
+    
+    
+}
